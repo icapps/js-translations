ec1d9d1861a6a331c32ae987ee950a148dd578af
hoetmaaiers <houdmeyers@gmail.com>
2016-02-08 19:33:43 +0100

    add example 'translations:list' task

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@ var importTranslations = require('./lib').import;
 var exec               = require('child_process').exec;
 
 gulp.task('translations:import', function(cb) {
-  importTranslations(
+  return importTranslations(
     'XXXXXXXXXXXXXXXXXXXXXX',
     {
       clean: true,
       verbose: true,
     }
-  ).then(cb);
+  );
+});
+
+
+// task to test the order of execution
+gulp.task('translations:list', ['translations:import'], function(cb) {
+  exec('ls ./src/locales', function(error, stdout, stderr) {
+    console.log('stdout: ', stdout);
+  });
 });
