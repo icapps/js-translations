da65985d9a988b16276a4004c1d94333f904c5a2
hoetmaaiers <houdmeyers@gmail.com>
2016-04-18 11:55:09 +0200

    add postinstall scripts hook

--- src/utils/prettifyJSON.js
+++ src/utils/prettifyJSON.js
@@ -1,6 +1,6 @@
 export default function prettifyJSON(obj) {
   // TODO: read .editorconfig inside the project for indenting preference
-  var string = JSON.stringify(obj, null, 2);
+  var string = JSON.stringify(obj, null, 4);
   // append newline to the end
   return string + '\n';
 }
